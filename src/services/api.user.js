05e0c5300b68308f105242e673905f92ad7c5c46
HieuTrannn <hieutran4525@gmail.com>
2025-03-11 12:33:20 +0700

    feat/hieu: add getUserById, updateSkinType and updateRole

--- src/services/api.user.js
+++ src/services/api.user.js
@@ -10,9 +10,40 @@ export const getUser = async () => {
   }
 };
 
+export const getUserById = async (id) => {
+  try {
+    const response = await api.get(`user/${id}`);
+    return response.data;
+  } catch (error) {
+    toast.error(error.response.data);
+  }
+};
+
 export const updateUser = async ({ id, user }) => {
   try {
     const response = await api.put(`user/${id}`, user);
+    return response.data;
+  } catch (error) {
+    toast.error(error.response.data);
+  }
+};
+
+export const updateSkinType = async ({ id, skinTypeEnum }) => {
+  try {
+    const response = await api.patch(`user/updateSkinType/${id}`, null, {
+      params: { skinTypeEnum }, // query parameter
+    });
+    return response.data;
+  } catch (error) {
+    toast.error(error.response.data);
+  }
+};
+
+export const updateRole = async ({ id, roleEnum }) => {
+  try {
+    const response = await api.patch(`user/updateRole/${id}`, null, {
+      params: { roleEnum }, // query parameter
+    });
     return response.data;
   } catch (error) {
     toast.error(error.response.data);
