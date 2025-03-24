1c7592414fa7348bfc9bc993e20a0b4beef552d4
HieuTrannn <hieutran4525@gmail.com>
2025-03-25 00:43:13 +0700

    fix/hieu: fix sidebar

--- src/pages/Comparison/SidebarComparison/index.jsx
+++ src/pages/Comparison/SidebarComparison/index.jsx
@@ -5,8 +5,10 @@ import api from "../../../config/axios";
 function CategorySidebar() {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { categoryId } = useParams(); // Lấy categoryId từ URL
+  const { category_id } = useParams(); // <-- tên đúng phải khớp với useParams trong ProductList
   const navigate = useNavigate();
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,34 +24,33 @@ function CategorySidebar() {
   }, []);
 
   return (
-    <div className="flex min-h-screen">
-      <div className="w-64 p-4 border-r bg-white shadow-md min-h-screen">
-        <h2 className="text-lg font-bold text-gray-800">Danh mục sản phẩm</h2>
+    <div className="w-64 p-4 border-r bg-white shadow-md min-h-screen">
+      <h2 className="text-lg font-bold text-gray-800">Danh mục sản phẩm</h2>
 
-        {loading ? (
-          <p className="text-gray-500 mt-2">Đang tải danh mục...</p>
-        ) : categories.length === 0 ? (
-          <p className="text-gray-500 mt-2">Không có danh mục nào.</p>
-        ) : (
-          <ul className="mt-4 space-y-2">
-            {categories.map((category) => (
-              <li key={category.id}>
-                <button
-                  className={`block w-full text-left p-2 rounded-md transition duration-300 ${
-                    Number(categoryId) === category.id
-                      ? "bg-blue-500 text-white font-semibold"
-                      : "text-gray-700 hover:bg-blue-100"
-                  }`}
-                  onClick={() => navigate(`/productList/${category.id}`)}
-                >
-                  {category.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      {loading ? (
+        <p className="text-gray-500 mt-2">Đang tải danh mục...</p>
+      ) : categories.length === 0 ? (
+        <p className="text-gray-500 mt-2">Không có danh mục nào.</p>
+      ) : (
+        <ul className="mt-4 space-y-2">
+          {categories.map((category) => (
+            <li key={category.id}>
+              <button
+                className={`block w-full text-left p-2 rounded-md transition duration-300 ${
+                  Number(category_id) === category.id
+                    ? "bg-blue-400 text-white font-semibold"
+                    : "text-gray-700 hover:bg-blue-100"
+                }`}
+                onClick={() => navigate(`/productList/${category.id}`)}
+              >
+                {category.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
+
 export default CategorySidebar;
